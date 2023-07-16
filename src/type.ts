132820eca1d64bc2b50d17c28ee915517636f094
bn-tw2020 <skaxodn97@gmail.com>
2023-07-16 11:00:38 +0900

    chore: metadata

--- src/type.ts
+++ src/type.ts
@@ -1,32 +1,60 @@
-export interface SiteSiteMetadata {
-    title: string;
-    author: Author;
-    siteUrl: string;
-    social: Social;
-  }
-  
-  export interface Author {
-    name: string;
-    summary: string;
-  }
-  
-  export interface Social {
-    twitter: string;
-  }
-  
-  export interface MarkdownRemark extends Node {
-    id: string;
-    frontmatter: Frontmatter;
-    fields: Fields;
-    excerpt: string;
-  }
-  
-  export interface Frontmatter {
-    title: string;
-    description: string;
-    date: string;
-  }
-  
-  export interface Fields {
-    slug: string;
-  }
+export interface SiteMetadata {
+  title: string;
+  author: Author;
+  siteUrl: string;
+  social: Social;
+  language: string;
+  comments: {
+    utterances: {
+      repo: string;
+    };
+  };
+}
+
+export interface Author {
+  name: string;
+  bio: {
+    role: string;
+    description: string[];
+    thumbnail: string;
+  };
+  social: Social;
+}
+
+export interface Social {
+  github: string;
+  linkedIn: string;
+  email: string;
+}
+
+export interface Post {
+  id: string;
+  excerpt: string;
+  html: string;
+  frontmatter: Frontmatter;
+  fields: Fields;
+}
+
+export interface AllMarkdownRemark {
+  edges: { node: MarkdownRemark; next: { fields: Fields }; prev: { fields: Fields } }[];
+}
+
+export interface MarkdownRemark {
+  id: string;
+  frontmatter: Frontmatter;
+  fields: Fields;
+  excerpt: string;
+  html: string;
+}
+
+export interface Frontmatter {
+  title: string;
+  author: string;
+  date: string;
+  emoji: string;
+  categories: string;
+}
+
+export interface Fields {
+  slug: string;
+}
