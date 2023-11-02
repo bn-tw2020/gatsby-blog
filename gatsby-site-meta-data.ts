0575d5e749fd8cdb19ec5041be5bcfc4347bf409
bn-tw2020 <skaxodn97@gmail.com>
2023-11-02 23:41:34 +0900

    Update github address

--- gatsby-site-meta-data.ts
+++ gatsby-site-meta-data.ts
@@ -2,11 +2,11 @@ export default {
   title: `개발자 스티치`,
   description: `개발자 스티치`,
   language: `ko`, // `ko`, `en` => currently support versions for Korean and English
-  siteUrl: `https://github.com/bn-tw2020`,
+  siteUrl: `https://github.com/taewooyo`,
   ogImage: `/og-image.png`, // Path to your in the 'static' folder
   comments: {
     utterances: {
-      repo: `bn-tw2020/gatsby-blog`,
+      repo: `taewooyo/gatsby-blog`,
     },
   },
   author: {
@@ -19,7 +19,7 @@ export default {
       bachelorDegree: 'Computer Software Engineering',
     },
     social: {
-      github: `https://github.com/bn-tw2020`,
+      github: `https://github.com/taewooyo`,
       linkedIn: `https://www.linkedin.com/in/태우-남-449403226/`,
       resume: ``,
     },
